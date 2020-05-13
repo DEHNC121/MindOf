43c717b7c64f2a95ee3e52de4af1de823a2fb1e4
DEHNC121 <59644945+DEHNC121@users.noreply.github.com>
2020-05-13 16:41:10 +0200

    Window (new)

--- src/com/mindofteam/game/Window.java
+++ src/com/mindofteam/game/Window.java
@@ -1,15 +1,17 @@
 package com.mindofteam.game;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Window extends JFrame
 {
     public Window ()
     {
+        Dimension ekran=Toolkit.getDefaultToolkit().getScreenSize();
         setTitle ("MindOf");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setContentPane (new GamePanel (1280, 680));
+        setContentPane (new GamePanel ((int)ekran.getWidth(), (int)ekran.getHeight()));
         pack();
 
         setLocationRelativeTo(null);
